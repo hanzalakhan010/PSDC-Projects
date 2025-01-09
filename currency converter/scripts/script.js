3f1b3530acfab0e4ac957f66f394d2f6d91be355
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-09 11:15:36 +0500

    Currency Converter App update

--- currency converter/scripts/script.js
+++ currency converter/scripts/script.js
@@ -1,5 +1,4 @@
 const apiKey = '774e48be10561ee8c5790932'
-
 function loadCurrencies(){
     console.log('fdfdfdf')
     fetch('../scripts/currencies.json')
@@ -8,10 +7,10 @@ function loadCurrencies(){
         console.log(data)
         for (ele in data.Currencies){
             document.getElementById('to').innerHTML+=`
-            <option value '${data.Currencies[ele][0]}'>${data.Currencies[ele][1]}</option>
+            <option value ='${data.Currencies[ele][0]}'>${data.Currencies[ele][1]}</option>
             `
             document.getElementById('from').innerHTML+=`
-            <option value '${data.Currencies[ele][0]}'>${data.Currencies[ele][1]}</option>
+            <option value ='${data.Currencies[ele][0]}'>${data.Currencies[ele][1]}</option>
             `
         }
     })
@@ -19,4 +18,17 @@ function loadCurrencies(){
 
 function fetchRates(){
     fetch(`https://v6.exchangerate-api.com/v6/${apiKey}/latest/USD`)
+    .then((res)=>res.json())
+    .then((data)=>{
+        localStorage.setItem('rates',JSON.stringify(data.conversion_rates))
+    })
+}
+
+function convert(){
+    let rates = JSON.parse(localStorage.getItem('rates'))
+    let to = document.getElementById('to').value
+    let from = document.getElementById('from').value
+    let currency = document.getElementById('currency').value
+    let convereted_curr = ((rates['USD']/rates[from])*rates[to]) * currency
+    document.getElementById('currency_converted').value =convereted_curr.toFixed(3)
 }
