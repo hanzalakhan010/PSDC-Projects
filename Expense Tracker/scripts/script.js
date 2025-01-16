9520cc312e50fb562b8ee809f24618a8d61c6696
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-16 11:40:02 +0500

    Update to expense tracker app

--- Expense Tracker/scripts/script.js
+++ Expense Tracker/scripts/script.js
@@ -1,9 +1,11 @@
-let expenses = []
+total = 0
 function addExpense(){
+    let expenses = JSON.parse(localStorage.getItem('expenses'))
     let expense = document.getElementById('expense').value
     let amount  = document.getElementById('amount').value
     let date = document.getElementById('date').value
     expenses.push({expense,amount,date})
+    total += parseInt(amount)
     document.getElementById('expenses').innerHTML+=`
     <tr>
         <td>${expense}</td>
@@ -11,18 +13,21 @@ function addExpense(){
         <td>${date}</td>
     </tr>    
     `
+    document.getElementById('total').innerHTML = `Total: ${total}`
     localStorage.setItem('expenses',JSON.stringify(expenses))
 }
 
 function LoadExpenses(){
     let expenses = JSON.parse(localStorage.getItem('expenses'))
-    expenses.array.forEach(element => {
+    expenses.forEach(element => {
+        total+=parseInt(element.amount)
         document.getElementById('expenses').innerHTML+=`
         <tr>
-            <td>${element[0]}</td>
-            <td>${element[1]}</td>
-            <td>${element[2]}</td>
+            <td>${element.expense}</td>
+            <td>${element.amount}</td>
+            <td>${element.date}</td>
         </tr>    
         `  
     });
+    document.getElementById('total').innerHTML = `Total: ${total}`
 }
