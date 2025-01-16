4f1442c04607599fdd1ff8080b27323a865af984
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-16 11:30:44 +0500

    Updated Expense tracker app

--- Expense Tracker/scripts/script.js
+++ Expense Tracker/scripts/script.js
@@ -1,6 +1,28 @@
+let expenses = []
 function addExpense(){
     let expense = document.getElementById('expense').value
     let amount  = document.getElementById('amount').value
     let date = document.getElementById('date').value
-    
+    expenses.push({expense,amount,date})
+    document.getElementById('expenses').innerHTML+=`
+    <tr>
+        <td>${expense}</td>
+        <td>${amount}</td>
+        <td>${date}</td>
+    </tr>    
+    `
+    localStorage.setItem('expenses',JSON.stringify(expenses))
+}
+
+function LoadExpenses(){
+    let expenses = JSON.parse(localStorage.getItem('expenses'))
+    expenses.array.forEach(element => {
+        document.getElementById('expenses').innerHTML+=`
+        <tr>
+            <td>${element[0]}</td>
+            <td>${element[1]}</td>
+            <td>${element[2]}</td>
+        </tr>    
+        `  
+    });
 }
