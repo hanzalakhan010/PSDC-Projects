839e34abb9b8bfc8e6937fb6dd018d3906b9bad3
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-21 10:27:32 +0500

    Update to RWD version of Calculator UI

---
+++ My Blog/scripts/script.js
@@ -0,0 +1,10 @@
+function toggleNav(){
+    if(document.getElementById('all').style.display == 'block') {
+        document.getElementById('all').style.display = 'none'
+    }
+    else{
+        document.getElementById('all').style.display = 'block'
+
+    }
+
+}
