514b450362f507efcfa576de58b7170dbd70b258
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-25 03:15:56 +0500

    Update to Resume builder

--- Resume Builder/scripts/script.js
+++ Resume Builder/scripts/script.js
@@ -1,5 +1,5 @@
-function addProject(){
-    document.querySelector('.projects').innerHTML+=`
+function addProject() {
+    document.querySelector('.projects').innerHTML += `
         <div class = 'project'>
             <input class = 'prName' placeholder="Project Name"></input>
             <input class = 'prLink'placeholder="Link to Project"></input>
@@ -8,30 +8,31 @@ function addProject(){
         </div>
     `
 }
-function addCertifate(){
-    document.querySelector('.certifications').innerHTML+=`
+function addCertifate() {
+    document.querySelector('.certifications').innerHTML += `
         <div class="certificate">
             <input type="text" placeholder="TITLE">
             <input type="text" placeholder="AWARDING BODY">
         </div>
     `
 }
-function saveProgress(){
+function saveProgress() {
     let certificates_array = []
     let name = document.getElementById('name').value
-    let adverb= document.getElementById('adverb').value
+    let adverb = document.getElementById('adverb').value
     let tel = document.getElementById('tel').value
     let email = document.getElementById('email').value
     let projects = document.getElementsByClassName('project')
     let skills = document.getElementsByClassName('skills')
     let certificates = document.getElementsByClassName('certificate')
-    localStorage.setItem('name',name)
-    localStorage.setItem('adverb',adverb)
-    localStorage.setItem('tel',tel)
-    localStorage.setItem('email',email)
-    for (certificate in certificates){
-        // certificates_array.push([certificates[certificate].children[0].value:certificates[certificate].children[1]])
-        console.log(certificate)
-    }
+    localStorage.setItem('name', name)
+    localStorage.setItem('adverb', adverb)
+    localStorage.setItem('tel', tel)
+    localStorage.setItem('email', email)
+    for (let index=0;index<certificates.length;index++){
+        console.log(index)
+        certificates_array.push([certificates.item(index).children.item(0).value, certificates.item(index).children.item(1).value])
+    };
+    localStorage.setItem('certificates', JSON.stringify(certificates_array))
 
 }
