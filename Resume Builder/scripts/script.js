3a52dfd388752b60ff926fd22b4ce075349ee00d
Hanzala Khan <pythondeveloper360@gmail.com>
2025-01-24 15:41:49 +0500

    Update to Resume builder

--- Resume Builder/scripts/script.js
+++ Resume Builder/scripts/script.js
@@ -1,3 +1,14 @@
 function addProject(){
+    document.querySelector('.projects').innerHTML+=`
+        <div class = 'project'>
+        <input class = 'prName' placeholder="Project Name"></input>
+        <input class = 'prLink'placeholder="Link to Project"></input>
+        <input class = 'prDesc' placeholder="Project Description"></input>
+        <input class = 'prTech'placeholder="Project Stack/technologies used"></input>
+        </div>
+    `
+}
+function saveProgress(){
     
+
 }
