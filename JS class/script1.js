3ea48e208ca225cbfb0d7f99482823fcab230b80
AbdRehm <abdulreh970@gmail.com>
2025-02-04 10:58:53 +0500

    Started JS Practice

---
+++ JS class/script1.js
@@ -0,0 +1,15 @@
+// name_= 'Hanzala Khan'
+// age  = 19
+// pie = 3.14
+// isStudying = true
+
+// food = undefined
+// drink = null
+
+// console.log(name_,age,pie,isStudying)
+
+
+// Question:
+// Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5,
+// between 2000 and 3200 (both included).
+// The numbers obtained should be printed in a comma-separated sequence on a single line.
