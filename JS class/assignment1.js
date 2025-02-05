32716a4982b1d01fc4a7036103a4c06f71280f4f
Hanzala Khan <pythondeveloper360@gmail.com>
2025-02-05 18:21:40 +0500

    Worked on js Assignment 2/4

--- JS class/assignment1.js
+++ JS class/assignment1.js
@@ -1,8 +1,38 @@
+// 2/4/25
 // Array vs object
 // var let const
-fruits  =[
 
+
+fruits  =[
+    'Banana',
+    'Appple',
+    'Orange'
 ]
+
+cart = [
+    {
+        productName:'pen',
+        quantity:10,
+        manafacturer:'Signature',
+        price:10
+    },
+    {
+        productName:'calculator',
+        quantity:1,
+        manafacturer:'Calola',
+        price:100
+    },
+    {
+        productName:'notepad',
+        quantity:2,
+        manafacturer:'gaba',
+        price:50
+    }
+]
+
+
+
+
 {
     name_ : 'Hanzala'
 }
