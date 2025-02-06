f5db36ade0efd3d2c7b93c637f3d9973fd436f36
= <=>
2025-02-06 11:33:38 +0500

    Js Class

--- JS class/assignment1.js
+++ JS class/assignment1.js
@@ -1,61 +1,75 @@
-// // 2/4/25
-// // Array vs object
-// // var let const
+// // // 2/4/25
+// // // Array vs object
+// // // var let const
 
 
-// fruits = [
-    //     'Banana',
-    //     'Appple',
-    //     'Orange'
-    // ]
-    //     var laptop = "HP Elite"
-    //     let manafacturer = "HP"
-    //     const pi = 3.14
-    //     // manafacturer outside this scope is undefined
-    // }console.log(obj1)
-var cart = {
-    pen: {
-        quantity: 10,
-        manafacturer: 'Signature',
-        price: 10
-    },
-    calculator: {
-        quantity: 1,
-        manafacturer: 'Calola',
-        price: 100
-    },
-    notepad: {
-        quantity: 2,
-        manafacturer: 'gaba',
-        price: 50
-    }
-}
+// // fruits = [
+//     //     'Banana',
+//     //     'Appple',
+//     //     'Orange'
+//     // ]
+//     //     var laptop = "HP Elite"
+//     //     let manafacturer = "HP"
+//     //     const pi = 3.14
+//     //     // manafacturer outside this scope is undefined
+//     // }console.log(obj1)
+// var cart = {
+//     pen: {
+//         quantity: 10,
+//         manafacturer: 'Signature',
+//         price: 10
+//     },
+//     calculator: {
+//         quantity: 1,
+//         manafacturer: 'Calola',
+//         price: 100
+//     },
+//     notepad: {
+//         quantity: 2,
+//         manafacturer: 'gaba',
+//         price: 50
+//     }
+// }
 
-function addToCart(item,quantity) {
-    if (cart.item){
-        cart.item.quantity++
-    }
+// function addToCart(item,quantity) {
+//     if (cart.item){
+//         cart.item.quantity++
+//     }
 
-}    
+// }    
     
 
-// console.log(laptop)console.log(obj1)
+// // console.log(laptop)console.log(obj1)
 
-// // console.log(manafacturer) // this will throw errorconsole.log(obj1)
+// // // console.log(manafacturer) // this will throw errorconsole.log(obj1)
     
 
 
-obj1 = {
-    name :'hanzala',
-    age:19,
-    addr:"karachi"
+// obj1 = {
+//     name :'hanzala',
+//     age:19,
+//     addr:"karachi"
+// }
+
+// obj2 = {...obj1}
+// console.log(obj1.name)
+// console.log(obj2.name)
+
+// obj1.name = 'Ubada'
+
+// console.log(obj1)
+// console.log(obj2)
+
+
+// var a =1 
+// var a = 3
+
+
+
+
+const a = {
+    name:  "hanzala"
 }
-
-obj2 = {...obj1}
-console.log(obj1.name)
-console.log(obj2.name)
-
-obj1.name = 'Ubada'
-
-console.log(obj1)
-console.log(obj2)
+a.name = 'Ubada'
+a.age = 12
+console.log(a)
