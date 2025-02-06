658ab9c660626af6293bf5ba776d892dc6d1518c
taufeeq0004 <zahid@gmail.com>
2025-02-06 10:27:38 +0500

    Challenge 1

--- JS class/assignment1.js
+++ JS class/assignment1.js
@@ -4,35 +4,39 @@
 
 
 // fruits = [
-//     'Banana',
-//     'Appple',
-//     'Orange'
-// ]
-// var cart = {
-//     pen: {
-//         quantity: 10,
-//         manafacturer: 'Signature',
-//         price: 10
-//     },
-//     calculator: {
-//         quantity: 1,
-//         manafacturer: 'Calola',
-//         price: 100
-//     },
-//     notepad: {
-//         quantity: 2,
-//         manafacturer: 'gaba',
-//         price: 50
-//     }
-// }
+    //     'Banana',
+    //     'Appple',
+    //     'Orange'
+    // ]
+    //     var laptop = "HP Elite"
+    //     let manafacturer = "HP"
+    //     const pi = 3.14
+    //     // manafacturer outside this scope is undefined
+    // }console.log(obj1)
+var cart = {
+    pen: {
+        quantity: 10,
+        manafacturer: 'Signature',
+        price: 10
+    },
+    calculator: {
+        quantity: 1,
+        manafacturer: 'Calola',
+        price: 100
+    },
+    notepad: {
+        quantity: 2,
+        manafacturer: 'gaba',
+        price: 50
+    }
+}
 
-// function addToCart() {
-//     var laptop = "HP Elite"
-//     let manafacturer = "HP"
-//     const pi = 3.14
-//     // manafacturer outside this scope is undefined
-// }console.log(obj1)
-    
+function addToCart(item,quantity) {
+    if (cart.item){
+        cart.item.quantity++
+    }
+
+}    
     
 
 // console.log(laptop)console.log(obj1)
@@ -50,16 +54,8 @@ obj1 = {
 obj2 = obj1
 console.log(obj1.name)
 console.log(obj2.name)
-// obj1 = {
-//     name:'Ubada'
-// }obj1 = {
-//     name:'Ubada'
-// }obj1 = {
-//     name:'Ubada'
-// }
-
 
 obj1.name = 'Ubada'
 
-console.log(obj1.name)
-console.log(obj2.name)
+console.log(obj1)
+console.log(obj2)
