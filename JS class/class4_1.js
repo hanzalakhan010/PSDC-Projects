21be2e256f013f1a9e332918c31edbbe82435801
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-02-13 06:21:49 +0000

    class 4_1

--- JS class/class4_1.js
+++ JS class/class4_1.js
@@ -1,31 +1,42 @@
 // IF ELSE 
 
-userLogin = -0
-if (userLogin){
-    console.log('if statement')
-}
-else{
-    console.log('if statement')
+// userLogin = -0
+// if (userLogin){
+//     console.log('if statement')
+// }
+// else{
+//     // console.log('if statement')
 
-}
+// }
 
 // Exeercise
 
 userLoggedIn = true
-userSubscription = 'pro'
+// userSubscription = 'pro'
+userSubscription = 'basic'
 
-if (userLoggedIn){
-    // console.log('')
-    if (userSubscription == 'pro'){
+if (userLoggedIn) {
+    if (userSubscription == 'pro') {
         console.log('Thanks, you are a pro subscriber')
     }
-    else{
-        console.log('Here is your data, consder subscribing to pro to get acces to pro features`')
+    else {
+        console.log('Here is your data, consder subscribing to pro to get acces to pro features')
     }
 }
-else{
+else {
     console.log('Please log in first')
 }
 // for loops
+
+
+// Marksheet
+// 90=a
+// 80=b
+// 70=c
+// 60=d
+// >60 =f
+
+
+
 
 
