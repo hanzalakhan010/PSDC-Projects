41dca25ed4c5fe509af45f0cdaeb62ed63b52a1c
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-02-13 07:06:34 +0000

    class 4_1

--- JS class/class4_1.js
+++ JS class/class4_1.js
@@ -15,17 +15,22 @@ userLoggedIn = true
 // userSubscription = 'pro'
 userSubscription = 'basic'
 
-if (userLoggedIn) {
-    if (userSubscription == 'pro') {
-        console.log('Thanks, you are a pro subscriber')
-    }
-    else {
-        console.log('Here is your data, consder subscribing to pro to get acces to pro features')
-    }
-}
-else {
-    console.log('Please log in first')
-}
+// if (userLoggedIn) {
+//     if (userSubscription == 'pro') {
+//         console.log('Thanks, you are a pro subscriber')
+//     }
+//     else {
+//         console.log('Here is your data, consder subscribing to pro to get acces to pro features')
+//     }
+// }
+// else {
+//     console.log('Please log in first')
+// }
+
+
+
+
+
 // for loops
 
 
@@ -36,7 +41,28 @@ else {
 // 60=d
 // >60 =f
 
+let marks = 90
+let grade 
+if (marks>=90){
+    grade = 'A'
+}
+if (marks>=80){
+    grade = "B"
+}
+if(marks>=70){
+    grade = 'C'
+}
+if(marks>=60){
+    grade = 'D'
+}
+else{
+    
+}
+if(marks<60){
 
+}
+
+// switch
 
 
 
---
+++ JS class/if_else.js
@@ -0,0 +1,95 @@
+// IF ELSE 
+
+// userLogin = -0
+// if (userLogin){
+//     console.log('if statement')
+// }
+// else{
+//     // console.log('if statement')
+
+// }
+
+// Exeercise
+
+// userLoggedIn = true
+// // userSubscription = 'pro'
+// userSubscription = 'basic'
+
+// if (userLoggedIn) {
+//     if (userSubscription == 'pro') {
+//         console.log('Thanks, you are a pro subscriber')
+//     }
+//     else {
+//         console.log('Here is your data, consder subscribing to pro to get acces to pro features')
+//     }
+// }
+// else {
+//     console.log('Please log in first')
+// }
+
+
+
+
+
+// for loops
+
+
+// Marksheet
+// 90=a
+// 80=b
+// 70=c
+// 60=d
+// >60 =f
+
+// let marks = 90
+// function marks(marks) {
+//     let grade
+//     if (marks >= 90) {
+//         grade = 'A'
+//         return grade
+
+//     }
+//     if (marks >= 80) {
+//         grade = "B"
+//         return grade
+//     }
+//     if (marks >= 70) {
+//         grade = 'C'
+//         return grade
+//     }
+//     if (marks >= 60) {
+//         grade = 'D'
+//         return grade
+//     }
+//     else {
+
+//     }
+//     if (marks < 60) {
+//         grade = 'F'
+//         return grade
+//     }
+// }
+function marks(marks) {
+    if (marks >= 60) {
+        grade = "D"
+    }
+    if (marks >= 70) {
+        grade = 'C'
+    }
+    if (marks >= 80) {
+        grade = "B"
+    }
+    if (marks >= 90) {
+        grade = 'A'
+
+    }
+    else {    
+        grade = 'F'
+    }
+    return grade
+}
+
+for (let i = 0; i <= 100; i++) {
+    mark = Math.floor(Math.random() * 100)
+    console.log(`Marks = ${mark} Grade = ${marks(mark)}`)
+}
