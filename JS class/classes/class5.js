2239b9ed2ee826b28382b29f60b815e0a536db76
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-02-18 06:17:46 +0000

    class 5

--- JS class/classes/class5.js
+++ JS class/classes/class5.js
@@ -5,22 +5,21 @@
 
 // for (let i = 1;i<=20;i++){
 //     console.log(i)
+// // }
+// n  = 10
+// sum = 0
+// for(let i = 1;i<=n;i++){
+//     sum+=i
 // }
-
-n  = 10
-sum = 0
-for(let i = 1;i<=n;i++){
-    sum+=i
-}
-console.log(sum)
+// console.log(sum)
 
 
 for (let i = 20;i>=1;i--){
     console.log(i)
 }
 
-for (let i=1;i<=40;i*=2){
-    if (i%5!=0){
-        console.log(i)
-    }
-}
+// for (let i=1;i<=40;i*=2){
+//     if (i%5!=0){
+//         console.log(i)
+//     }
+// }
