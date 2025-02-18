329d5e1a92a6fe93f8a29199cfde1c62d68ecf8c
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-02-18 04:44:47 +0000

    switchStatement

--- JS class/Assignments/switchStatement.js
+++ JS class/Assignments/switchStatement.js
@@ -1,49 +1,52 @@
 
 
-// grade
-// switch(true){
-//     case mark>=90:{
-//         grade = 'A'
-//         break
-//     }
-//     case mark>=80:{
-//         grade = "B"
-//         break
-//     }
-//     case mark>=70:{
-//         grade = "C"
-//         break
-//     }
-//     case mark>=60:{
-//         grade = 'D'
-//         break
-//     }
-//     default:{
-//         grade = 'F'
-//     }
-// }
-// for (let i = 0; i <= 100; i++) {
-//     let mark = Math.floor(Math.random() * 100)
-//     // result.push([mark, marks(mark)])
-//     console.log(`Marks = ${mark} Grade = ${marks(mark)}`)
-// }
+function marks(mark){
+    var grade = 'defa'
+    switch(true){
+    case mark>=90:{
+        grade = 'A'
+        return grade
+    }
+    case mark>=80:{
+        grade = "B"
+        return grade
+    }
+    case mark>=70:{
+        grade = "C"
+        return grade
+    }
+    case mark>=60:{
+        grade = 'D'
+        return grade
+    }
+    default:{
+        grade = 'F'
+        return grade
 
-
-let trafficLight = 'green'
-let message = ''
-
-switch(trafficLight){
-    case "green":
-        message = 'proceed to drive'
-    
-    case 'yellow':
-        message = 'prepare to stop'
-    
-    case "red":
-        message = 'Stop Immediately'
-    
-    default:
-        message = 'Invalid color'
-    
+    }
 }
-console.log(message)
+}
+for (let i = 0; i <= 100; i++) {
+    let mark = Math.floor(Math.random() * 100)
+    console.log(`Marks = ${mark} Grade = ${marks(mark)}`)
+}
+
+
+// let trafficLight = 'green'
+// let message = ''
+
+// switch(trafficLight){
+//     case "green":
+//         message = 'proceed to drive'
+    
+//     case 'yellow':
+//         message = 'prepare to stop'
+    
+//     case "red":
+//         message = 'Stop Immediately'
+    
+//     default:
+//         message = 'Invalid color'
+    
+// }
+// console.log(message)
