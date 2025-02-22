4ac521060c07793a83298fd8c51fbe3fc9dc7053
Hanzala Khan <pythondeveloper360@gmail.com>
2025-02-22 08:02:38 +0500

    fixed bubble sort

--- JS class/Out of class Practices/buble sort.js
+++ JS class/Out of class Practices/buble sort.js
@@ -3,15 +3,17 @@ for (let i = 0;i<=10;i++){
     arr.push(Math.round(Math.random()*10))
     // arr.push(i)
 }
-// console.log(arr)
+console.log(arr)
 
 
 function bubbleSort(arr){
     for (let i=0;i<=arr.length;i++){
         // console.log(arr[i])
-        for(let j =0;j<i;j++){
+        for(let j =0;j<=i;j++){
             if (arr[j]>arr[j+1]){
+                temp = arr[j+1]
                 arr[j+1] = arr[j]
+                arr[j] = temp
             }
         }
     }
