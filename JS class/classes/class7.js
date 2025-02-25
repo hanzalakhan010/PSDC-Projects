8b32395df02821b62673363c4217131a80f698e7
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-02-25 05:13:15 +0000

    for in

--- JS class/classes/class7.js
+++ JS class/classes/class7.js
@@ -12,35 +12,41 @@
 // for(lel i =0;i<sen.length;)
 
 let email = 'hanzalakhan.private@gmail.com'
-tokens = email.split('@')
-let newEmail = ''
-if (tokens[0].length>2){
-    // for(let i=0;i<tokens[0].length;i++){
-    //     newEmail+=
-    // }
+function emailMasking(email){
 
-    newEmail+=tokens[0][0]
-    for(let i = 0;i<tokens[0].length-2;i++)
-        newEmail+='*'
-    newEmail+=tokens[0][tokens[0].length-1]
-    newEmail+='@'
-
-
+    tokens = email.split('@')
+    let newEmail = ''
+    if (tokens[0].length>2){
+        // for(let i=0;i<tokens[0].length;i++){
+        //     newEmail+=
+        // }
+    
+        newEmail+=tokens[0][0]
+        for(let i = 0;i<tokens[0].length-2;i++)
+            newEmail+='*'
+        newEmail+=tokens[0][tokens[0].length-1]
+        newEmail+='@'
+    
+    
+    }
+    else{
+        newEmail = email
+    }
+    newEmail+=tokens[1]
 }
-else{
-    newEmail = email
-}
 
-newEmail+=tokens[1]
-console.log(newEmail)
+
 
 // 3d array
 // values accession
+
+
 let str = "Hanzala Khan"
 let newStr = ''
 for (let chr of str){
     newStr = chr+newStr
 }
+
 // console.log(newStr)
 
 // arr = [1,2,3,4]
@@ -58,7 +64,9 @@ obj = {
     d:4
 }
 
-
-for (let key in obj){
-    console.log(obj[key])
-}
+a = 'b'
+console.log(obj.a)
+// for (let key in obj){
+//     console.log(obj[key])
+//     console.log(obj[key])
+// }
