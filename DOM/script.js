0867cb8ffaea460fb0f45a4bffc94bfbdc53d04d
= <=>
2025-02-27 11:46:47 +0500

    DOM

---
+++ DOM/script.js
@@ -0,0 +1,15 @@
+// var div = document.getElementById('div')
+
+// // div.textContent = 'Hanzala Khan'
+// // div.textContent = '<p>Hanzala Khan</p>'
+// // div.innerText  = "<p>Hanzala Khan</p>"
+// // div.innerHTML = '<p>Hanzala Khan</p>'
+
+// console.log(div.textContent)
+// console.log(div.innerHTML)
+// console.log(div.innerText)
+
+// document.query
+
+document.getElementById('paragraph').textContent = 'New paragraph text'
+document.getElementById('heading').innerText = 'New heading text'
