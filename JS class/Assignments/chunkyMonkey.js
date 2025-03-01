23dbb5f498d95b50b95ca6840753cc158f48e41d
Hanzala Khan <46111633+hanzalakhan010@users.noreply.github.com>
2025-03-01 20:45:32 +0000

    tested chunky monkey

--- JS class/Assignments/chunkyMonkey.js
+++ JS class/Assignments/chunkyMonkey.js
@@ -8,4 +8,11 @@ function chunkyMonkey(arr,size){
 // size = 3
 // length = 9
 
-console.log(chunkyMonkey([1,2,3,4,5,6,8,9],3))
+// console.log(chunkyMonkey([1,2,3,4,5,6,8,9],3))
+console.log(chunkyMonkey([
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ],5))
